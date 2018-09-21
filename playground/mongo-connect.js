e97dde190a47cc500f34144081c7a253b05e97b9
mats001 <anilmathew001@gmail.com>
2018-09-21 23:07:02 +0400

    Mongo db

--- playground/mongo-connect.js
+++ playground/mongo-connect.js
@@ -6,37 +6,37 @@ MongoClient.connect('mongodb://localhost:27017/TodoAppData',(err,client)=>{
     }
     const db=client.db('TodoAppData');
     console.log('Succesfully Connected to Mongo Server');
-const insertRec =(text,compFlag)=>{
-  db.collection('Todos').insertOne({
-     text:text,
-     complete:compFlag,
-   },(err,result)=>{
-     if (err){
-       return console.log('Unable to create collection Tods');
-     }
-     console.log(JSON.stringify(result.ops,undefined,2));
-   });
-}
-  for (let i=1;i<10;i++){
-      let text=`This is my Todo number ${i}`
-      if (i==1||i==5||i==8){
-        compFlag=false
-      }else{
-        compFlag=true;
-      }
-      insertRec(text,compFlag)
-  }
+// const insertRec =(text,compFlag)=>{
+//   db.collection('Todos').insertOne({
+//      text:text,
+//      complete:compFlag,
+//    },(err,result)=>{
+//      if (err){
+//        return console.log('Unable to create collection Tods');
+//      }
+//      console.log(JSON.stringify(result.ops,undefined,2));
+//    });
+// }
+//   for (let i=1;i<10;i++){
+//       let text=`This is my Todo number ${i}`
+//       if (i==1||i==5||i==8){
+//         compFlag=false
+//       }else{
+//         compFlag=true;
+//       }
+//       insertRec(text,compFlag)
+//   }
 
-    // db.collection('Users').insertOne({
-    //   name:'Vinod Mathew',
-    //   age:51,
-    //   location:'Dammam, Saudi Arabia'
-    // },(err,result)=>{
-    //   if (err){
-    //     return console.log('Could not insert to Collection User',err)
-    //   }
-    //   console.log(JSON.stringify(result.ops,undefined,2));
-    // });
+    db.collection('Users').insertOne({
+      name:'Anil Mathew',
+      age:49,
+      location:'Dubai, UAE'
+    },(err,result)=>{
+      if (err){
+        return console.log('Could not insert to Collection User',err)
+      }
+      console.log(JSON.stringify(result.ops,undefined,2))
+    });
 
     client.close();
 });
--- playground/mongo-find.js
+++ playground/mongo-find.js
@@ -6,15 +6,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoAppData',(err,client)=>{
     }
     const db=client.db('TodoAppData');
     console.log('Succesfully Connected to Mongo Server');
-    db.collection('Todos').find({
-      //_id: new ObjectID('5b6dc0b46cbbfb7228bbcd64')
-      text:'Talk the  talk'
-    }).toArray()
+    db.collection('Todos').find({complete:true}).toArray()
       .then((docs)=>{
         console.log('Todos');
         console.log(JSON.stringify(docs,undefined,2));
       },(err)=>{
         console.log('Unable to connect to data',err)
-    })
-     // client.close();
+    }).then(()=>{
+      console.log("Calling close")
+      client.close();
+    });
 });
