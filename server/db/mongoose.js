2f1a37330feedc26da8bfd467ac69c319997f2e6
mats001 <anilmathew001@gmail.com>
2018-09-30 23:39:55 +0400

    Mongodb on mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const mlabDbase = 'mongodb://anilmathew001:aAli0423@ds119523.mlab.com:19523/todo-api'
+const mlabDbase = 'mongodb://admin:admin123@ds119523.mlab.com:19523/todo-api'
 mongoose.connect(mlabDbase||'mongodb://127.0.0.1:27017/TodoAppData');
 
 module.exports={
